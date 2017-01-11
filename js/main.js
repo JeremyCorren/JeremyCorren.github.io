78916e6603fa23aba82429d62fba508ab1cc3aeb
Jeremy Corren <jeremycorrenmusic@gmail.com>
2017-01-11 17:35:49 -0500

    Added jQuery fade-in/out functionality

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
-	
 	$("#meat").css("display", "none");
-
-    $("#meat").fadeIn(2000);
+    $("#meat").fadeIn(1000);
     
-	$("a.transition").click(function(event){
+	$("a").click(function(event){
 		event.preventDefault();
 		linkLocation = this.href;
 		$("#meat").fadeOut(1000, redirectPage);		
